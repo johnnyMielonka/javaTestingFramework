0d6cc2d22668577c7bb5818594c934e225edd75d
Aaarek <arkadiusz.piskorz@gmail.com>
2024-05-29 21:46:52 +0200

    BasePage cleanup

--- src/main/java/com/automation/pages/BasePage.java
+++ src/main/java/com/automation/pages/BasePage.java
@@ -1,7 +1,9 @@
 package com.automation.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -10,21 +12,41 @@ import java.time.Duration;
 public class BasePage extends PageGenerator {
     public static final int PAGE_LOAD_TIMEOUT = 15;
 
-    public BasePage(WebDriver driver) {
+    protected BasePage(WebDriver driver) {
         super(driver);
     }
 
-    public void openUrl(String url) {
+    protected void openUrl(String url) {
         driver.get(url);
         this.waitForPageLoad();
     }
 
-    public void waitForPageLoad() {
+    protected void waitForPageLoad() {
         ExpectedCondition<Boolean> pageLoadStatus = driver1 -> ((JavascriptExecutor) driver)
                         .executeScript("return document.readyState")
                         .equals("complete");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(PAGE_LOAD_TIMEOUT));
         wait.until(pageLoadStatus);
+    }
+
+    protected void click (WebElement element){
+        this.waitForPageLoad();
+        element.click();
+    }
+    protected void writeText(WebElement element, String textValue){
+        this.waitForPageLoad();
+        element.clear();
+        element.sendKeys(textValue);
+    }
+
+    protected String getText(WebElement element){
+        this.waitForPageLoad();
+        return element.getText();
+    }
+
+    protected boolean isVisible(By element){
+        this.waitForPageLoad();
+        return !super.driver.findElements(element).isEmpty();
     }
 
 }
