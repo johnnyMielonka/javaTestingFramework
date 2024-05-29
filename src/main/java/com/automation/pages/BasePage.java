0c0afff9c3f564056602c6bd15d6c347100a9ea9
Aaarek <arkadiusz.piskorz@gmail.com>
2024-05-29 23:38:29 +0200

    adding fluent wait

--- src/main/java/com/automation/pages/BasePage.java
+++ src/main/java/com/automation/pages/BasePage.java
@@ -1,16 +1,17 @@
 package com.automation.pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class BasePage extends PageGenerator {
     public static final int PAGE_LOAD_TIMEOUT = 15;
+    public static final int WAIT_TIMEOUT = 10;
+    public static final int WAIT_POLLING = 1;
 
     protected BasePage(WebDriver driver) {
         super(driver);
@@ -23,17 +24,24 @@ public class BasePage extends PageGenerator {
 
     protected void waitForPageLoad() {
         ExpectedCondition<Boolean> pageLoadStatus = driver1 -> ((JavascriptExecutor) driver)
-                        .executeScript("return document.readyState")
-                        .equals("complete");
+                .executeScript("return document.readyState")
+                .equals("complete");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(PAGE_LOAD_TIMEOUT));
         wait.until(pageLoadStatus);
     }
 
-    protected void click (WebElement element){
-        this.waitForPageLoad();
-        element.click();
+    private FluentWait<WebDriver> waitUntil(Duration timeout, Duration polling) {
+        return new FluentWait<>(driver)
+                .withTimeout(timeout)
+                .pollingEvery(polling)
+                .ignoring(NoSuchElementException.class);
     }
-    protected void writeText(WebElement element, String textValue){
+
+    protected FluentWait<WebDriver> waitUntil() {
+        return this.waitUntil(Duration.ofSeconds(WAIT_TIMEOUT), Duration.ofSeconds(WAIT_POLLING));
+    }
+
+    protected void click(By element) {
         this.waitForPageLoad();
         element.clear();
         element.sendKeys(textValue);
@@ -44,7 +52,7 @@ public class BasePage extends PageGenerator {
         return element.getText();
     }
 
-    protected boolean isVisible(By element){
+    protected boolean isVisible(By element) {
         this.waitForPageLoad();
         return !super.driver.findElements(element).isEmpty();
     }
