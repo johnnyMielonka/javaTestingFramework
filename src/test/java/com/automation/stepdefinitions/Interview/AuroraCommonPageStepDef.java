581ee8d575ea20e09cffe0733b974aa87978ba70
Aaarek <arkadiusz.piskorz@gmail.com>
2024-05-29 21:54:01 +0200

    adding Step Definitions for Aurora Commerce

---
+++ src/test/java/com/automation/stepdefinitions/Interview/AuroraCommonPageStepDef.java
@@ -0,0 +1,50 @@
+package com.automation.stepdefinitions.Interview;
+
+import com.automation.core.DriverFactory.PageManager;
+import com.automation.pages.PageGenerator;
+import com.automation.pages.pageObjects.Interview.AuroraCommonPage;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.assertj.core.api.SoftAssertions;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class AuroraCommonPageStepDef {
+    private final PageGenerator pageGenerator = PageManager.getInstance().getPageGenerator();
+    private final AuroraCommonPage mainPage = pageGenerator.getInstance(AuroraCommonPage.class);
+    private final SoftAssertions softAssertions = new SoftAssertions();
+
+    @Given("Open Aurora web page")
+    public void startingPoint() {
+        mainPage.openServiceUrl();
+    }
+
+    @When("Open an {string} item from Menu Bar")
+    public void openItemMenuBar(String itemName) {
+        mainPage.clickOnManuBarItem(itemName);
+    }
+
+    @Then("Verify page title is {string}")
+    public void verifyPageTitle(String expectedName) {
+        assertEquals("Page name different then expected", mainPage.getPageName(), expectedName);
+    }
+
+    @Then("Verify page title is {string} with uppercases")
+    public void verifyPageTitleUpperCases(String expectedName) {
+        verifyPageTitle(expectedName.toUpperCase());
+    }
+
+    @Then("Verify Menu Bar contains following items:")
+    public void sofAssertionExample(DataTable inputTable) {
+        for (List<String> row : inputTable.asLists()) {
+            softAssertions.assertThat(mainPage.checkMenuBarItemVisible(row.get(0))).isEqualTo(row.get(0));
+        }
+        softAssertions.assertAll();
+    }
+
+}
---
+++ src/test/java/com/automation/stepdefinitions/Interview/AuroraLoginPageStepDef.java
@@ -0,0 +1,47 @@
+package com.automation.stepdefinitions.Interview;
+
+import com.automation.core.DriverFactory.PageManager;
+import com.automation.pages.PageGenerator;
+import com.automation.pages.pageObjects.Interview.AuroraCommonPage;
+import com.automation.pages.pageObjects.Interview.AuroraLoginPage;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.assertj.core.api.SoftAssertions;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class AuroraLoginPageStepDef {
+    private final PageGenerator pageGenerator = PageManager.getInstance().getPageGenerator();
+    private final AuroraLoginPage loginPage = pageGenerator.getInstance(AuroraLoginPage.class);
+    private final AuroraCommonPage mainPage = pageGenerator.getInstance(AuroraCommonPage.class);
+    private final SoftAssertions softAssertions = new SoftAssertions();
+
+    @Given("Navigate to login page")
+    public void navigateToLoginPage() {
+        loginPage.navigateToLoginPage();
+    }
+
+    @When("Log in as {string} user and {string} password")
+    public void loginAsUserPassword(String user, String password) {
+        loginPage.setEmailForLogin(user)
+                .setPasswordForLogin(password)
+                .pressLogIn();
+    }
+
+    @Then("Verify if error popup is visible")
+    public void verifyErrorPopupIsVisible() {
+        assertTrue("No error is visible", mainPage.isErrorPopupVisible());
+    }
+
+    @Then("Verify error popup exist with text:")
+    public void verifyErrorPopupHasText(String expectedErrorText) {
+        assertEquals(
+                expectedErrorText.toLowerCase(),
+                mainPage.getErrorPopupText().toLowerCase());
+    }
+}
